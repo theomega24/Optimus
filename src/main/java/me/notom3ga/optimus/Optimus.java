89b8fa811084eb2b8ee41387c2ced3fb73fdd497
Ben Kerllenevich <me@notom3ga.me>
2021-04-07 17:38:04 -0400

    require paper/adventure/1.16

--- src/main/java/me/notom3ga/optimus/Optimus.java
+++ src/main/java/me/notom3ga/optimus/Optimus.java
@@ -9,7 +9,9 @@ import me.notom3ga.optimus.command.impl.RecalculateCommand;
 import me.notom3ga.optimus.config.Config;
 import me.notom3ga.optimus.listener.PlayerListener;
 import me.notom3ga.optimus.packet.queue.PacketQueue;
+import me.notom3ga.optimus.util.Constants;
 import me.notom3ga.optimus.util.Logger;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Optimus extends JavaPlugin {
@@ -23,6 +25,28 @@ public class Optimus extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        try {
+            Class.forName("com.destroystokyo.paper.PaperConfig");
+        } catch (ClassNotFoundException e) {
+            Logger.severe("Failed to load Optimus v" + Constants.VERSION);
+            Logger.severe("We require Paper (https://papermc.io/)");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        try {
+            Class.forName("net.kyori.adventure.audience.Audience");
+        } catch (ClassNotFoundException e) {
+            Logger.severe("Failed to load Optimus v" + Constants.VERSION);
+            Logger.severe("We require Paper with Adventure included");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        if (!Bukkit.getServer().getClass().getPackage().getName().contains(Constants.NMS_REVISION)) {
+            Logger.severe("Failed to load Optimus v" + Constants.VERSION);
+            Logger.severe("We require NMS Revision " + Constants.NMS_REVISION);
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
         Config.load();
         try {
             this.commandManager = new CommandManager();
--- src/main/java/me/notom3ga/optimus/util/Constants.java
+++ src/main/java/me/notom3ga/optimus/util/Constants.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class Constants {
     public static final String VERSION = "0.0-ALPHA";
+    public static final String NMS_REVISION = "v1_16_R3";
 
     public static final TextColor BRAND_COLOR = TextColor.fromHexString("#A3A8F0");
     public static final TextColor HIGHLIGHT = TextColor.fromHexString("#D6D6D6");
